dc78283c86ab6ac8f88bda7144c49c84b6e82672
Henrique Recidive <recidive@gmail.com>
2016-07-02 14:13:47 -0300

    Moved server started message to a middleware. Added baseDir to state.

--- modules/server/index.js
+++ modules/server/index.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import {BOOT} from 'redux-boot'
 import {HTTP_BOOT, HTTP_AFTER_BOOT} from 'redux-boot-express'
 import express from 'express'
 import React from 'react'
@@ -7,10 +8,10 @@ import Html from './components/Html';
 
 export default {
   reducer: {
-    [HTTP_AFTER_BOOT]: (state, action) => {
-      const port = state.variables.port
-      console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-      return state
+    [BOOT]: (state, action) => {
+      return Object.assign({}, state, {
+        baseDir: path.join(__dirname, '../..')
+      });
     }
   },
   middleware: {
@@ -23,8 +24,9 @@ export default {
           main: "/static/bundle.js"
         }
       }
+      const baseDir = store.getState().baseDir
 
-      httpServer.use('/static', express.static(path.join(__dirname, '../..', 'dist')));
+      httpServer.use('/static', express.static(path.join(baseDir, 'dist')));
 
       httpServer.use((request, response, next) => {
 
@@ -35,6 +37,13 @@ export default {
 
         next();
       })
+
+      return next(action)
+    },
+    [HTTP_AFTER_BOOT]: store => next => action => {
+      const port = store.getState().variables.port
+
+      console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
 
       return next(action)
     }
